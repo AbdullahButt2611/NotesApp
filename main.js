4f3e5bdbdfcf933962075319a07c7c36f2a44728
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-08-14 08:18:41 +0500

    Complete
    All the functionalities and operations has been added to this app

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
+const popupTitle = document.querySelector("header p");
 const closeIcon = popupBox.querySelector("header i");
 const titleTag = popupBox.querySelector("input");
 const descTag = popupBox.querySelector("textarea");
 const addBtn = popupBox.querySelector("button");
 
+
+
+
+
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
 
 // Getting local storage notes if exist and parsing them
 //to JS object else passing the empty array to notes.
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
 
+let isUpdate = false, updateId;
+
+
+
+
+
+
+
+
+
+
+
+
 // Display the form on clicking the Add Note Box
 addBox.addEventListener("click", () => {
 
     popupBox.classList.add("show");
+    titleTag.focus();
 
 });
+
+
+
+
+
+
 
 
 // Closing the opened form
@@ -25,21 +53,46 @@ closeIcon.addEventListener("click", () => {
 
     titleTag.value = "";
     descTag.value = "";
+    addBtn.innerText = "Add Note"
+    popupTitle.innerText = "Add a New Note"
+    isUpdate = false;
     popupBox.classList.remove("show");
 
 });
 
+
+
+
+
+
+
+
 function showMenu(elem){
     elem.parentElement.classList.add("show");
+
+    // Removing show class from menu on document click
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != elem)
+        {
+            elem.parentElement.classList.remove("show");
+        }
+    });
 }
 
 
+
+
+
+
+
+// Function that will display all the notes on the screen by refreshing the 
+//page and fetching data from local storage
 function showNotes(){
 
     // Removing the duplication of elements if exist
     document.querySelectorAll(".note").forEach(note => note.remove());
 
-    notes.forEach((note) => {
+    notes.forEach((note, index) => {
 
         let liTag = `<li class="note">
                         <p>${note.title}</p>
@@ -54,8 +107,8 @@ function showNotes(){
                             <div class="settings">
                                 <i onclick = "showMenu(this)" class='bx bx-dots-horizontal-rounded' ></i>
                                 <ul class="menu">
-                                    <li><i class='bx bx-pencil'></i>Edit</li>
-                                    <li><i class='bx bx-trash' ></i>Delete</li>
+                                    <li onclick = "updateNote(${index}, '${note.title}', '${note.description}')"><i class='bx bx-pencil'></i>Edit</li>
+                                    <li onclick="deleteNote(${index})"><i class='bx bx-trash' ></i>Delete</li>
                                 </ul>
                             </div>
                         </div>
@@ -66,6 +119,14 @@ function showNotes(){
 }
 
 showNotes();
+
+
+
+
+
+
+
+
 
 
 
@@ -89,8 +150,16 @@ addBtn.addEventListener("click", e => {
             date: `${month} ${day}, ${year}`
         }
 
+        if(!isUpdate)
+        {
+            notes.push(noteInfo);       //Adding new note to notes.
+        }
+        else
+        {   
+            isUpdate = false;
+            notes[updateId] = noteInfo;             //Updating the specified note.
+        }
         
-        notes.push(noteInfo);       //Adding new note to notes.
 
         //Saving notes to locaql storage
         localStorage.setItem("notes", JSON.stringify(notes))
@@ -100,3 +169,42 @@ addBtn.addEventListener("click", e => {
     }
 
 });
+
+
+
+
+
+
+
+
+
+// Function to delete a note
+function deleteNote(noteId){
+
+    let confirmDelete = confirm("Are you sure you want to delete this note?");
+    if(!confirmDelete) return;
+
+    notes.splice(noteId, 1)                 //removing selected notes from array tasks
+
+    //Saving updated notes to locaql storage
+    localStorage.setItem("notes", JSON.stringify(notes))
+    showNotes();
+}
+
+
+
+
+
+
+
+
+function updateNote(noteId, title, description)
+{
+    isUpdate = true;
+    updateId = noteId;
+    addBox.click();
+    addBtn.innerText = "Update Note"
+    popupTitle.innerText = "Update a Note"
+    titleTag.value = title;
+    descTag.value = description;
+}
